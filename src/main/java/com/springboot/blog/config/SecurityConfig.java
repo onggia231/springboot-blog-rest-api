e278bce838d2f95745c70c8162d0c9847d9680bf
Bùi Nam Ngọc <ngocbn@telsoft.com.vn>
2024-06-12 23:35:19 +0700

    NgocBn commit code SecurityConfig

--- src/main/java/com/springboot/blog/config/SecurityConfig.java
+++ src/main/java/com/springboot/blog/config/SecurityConfig.java
@@ -1,23 +1,25 @@
 package com.springboot.blog.config;
 
+import com.springboot.blog.security.CustomUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
+
+  @Autowired
+  private CustomUserDetailsService userDetailsService;
 
   @Bean
   public PasswordEncoder passwordEncoder() {
@@ -36,18 +38,22 @@ public class SecurityConfig {
     return http.build();
   }
 
-  @Bean
-  public UserDetailsService userDetailsService() {
-    UserDetails namngoc = User.builder()
-            .username("namngoc")
-            .password(passwordEncoder().encode("123456"))
-            .roles("USER")
-            .build();
-    UserDetails admin = User.builder()
-            .username("admin")
-            .password(passwordEncoder().encode("admin"))
-            .roles("ADMIN")
-            .build();
-    return new InMemoryUserDetailsManager(namngoc, admin);
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
   }
+
+//  @Bean
+//  public UserDetailsService userDetailsService() {
+//    UserDetails namngoc = User.builder()
+//            .username("namngoc")
+//            .password(passwordEncoder().encode("namngoc"))
+//            .roles("USER")
+//            .build();
+//    UserDetails admin = User.builder()
+//            .username("admin")
+//            .password(passwordEncoder().encode("admin"))
+//            .roles("ADMIN")
+//            .build();
+//    return new InMemoryUserDetailsManager(namngoc, admin);
+//  }
 }
